2b0bd5958f4e53b129bd981b1e24d5777d8107bc
Laurent Dutheil <ldutheil74@gmail.com>
2025-12-03 15:33:24 +0100

    day2 part 2

--- day2/validator.go
+++ day2/validator.go
@@ -1,15 +1,33 @@
 package day2
 
-import "strconv"
+import (
+	"strconv"
+)
+
+var primes = []int{2, 3, 5, 7, 11}
 
 func Validate(id int) bool {
 	sId := strconv.Itoa(id)
-	i := len(sId) / 2
-	return !(sId[:i] == sId[i:])
+	if len(sId) == 1 {
+		return true
+	}
+
+	for _, div := range primes {
+		i := len(sId) / div
+		result := len(sId)%div == 0
+		for j := 0; j < div-1; j++ {
+			result = result && sId[j*i:(j+1)*i] == sId[(j+1)*i:(j+2)*i]
+		}
+		if result {
+			return false
+		}
+	}
+
+	return true
 }
 
 func ValidateInterval(interval []int) []int {
-	var result []int
+	var result = make([]int, 0)
 	for i := interval[0]; i <= interval[1]; i++ {
 		if !Validate(i) {
 			result = append(result, i)
--- day2/validator_test.go
+++ day2/validator_test.go
@@ -17,8 +17,13 @@ func TestValidate(t *testing.T) {
 		{"Odd length IDs are valid", 3, true},
 		{"11 is invalid", 11, false},
 		{"12 is valid", 12, true},
+		{"111 is invalid", 111, false},
 		{"1010 is invalid", 1010, false},
 		{"222222 is invalid", 222222, false},
+		{"565656 is invalid", 565656, false},
+		{"1111111 is invalid", 1111111, false},
+		{"2121212121 is invalid", 2121212121, false},
+		{"55555555555 is invalid", 55555555555, false},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
@@ -33,9 +38,17 @@ func TestValidateInterval(t *testing.T) {
 		interval []int
 		expected []int
 	}{
-		{"95-115 has one invalid ID", []int{95, 115}, []int{99}},
 		{"11-22 has two invalid IDs", []int{11, 22}, []int{11, 22}},
+		{"95-115 has one invalid ID", []int{95, 115}, []int{99, 111}},
+		{"998-1012 has one invalid ID", []int{998, 1012}, []int{999, 1010}},
 		{"1188511880-1188511890 has one invalid ID", []int{1188511880, 1188511890}, []int{1188511885}},
+		{"222220-222224 has one invalid ID", []int{222220, 222224}, []int{222222}},
+		{"1698522-1698528 has no invalid ID", []int{1698522, 1698528}, []int{}},
+		{"446443-446449 has one invalid ID", []int{446443, 446449}, []int{446446}},
+		{"38593856-38593862 has one invalid ID", []int{38593856, 38593862}, []int{38593859}},
+		{"565653-565659 now has one invalid ID", []int{565653, 565659}, []int{565656}},
+		{"824824821-824824827 now has one invalid ID", []int{824824821, 824824827}, []int{824824824}},
+		{"2121212118-2121212124 now has one invalid ID", []int{2121212118, 2121212124}, []int{2121212121}},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
@@ -54,5 +67,5 @@ func TestFile(t *testing.T) {
 			result += invalidId
 		}
 	}
-	assert.Equal(t, 30599400849, result)
+	assert.Equal(t, 46270373595, result)
 }
